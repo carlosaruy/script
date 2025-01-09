704a40e50f9270e6bd2be2f7f5977ee177317e92
carlosuy <carlosarmstrong@gmail.com>
2025-01-09 11:34:24 -0300

    creates directory entry object to use directorySearch object

--- enumeration.ps1
+++ enumeration.ps1
@@ -21,4 +21,13 @@ $DN = ([adsi]'').distinguishedName
 # if the port is not the default, the path must be changed 
 
 $LDAP = "LDAP://$PDC/$DN"
-$LDAP
+#$LDAP
+
+# creates a directoryEntry Object, referenciates to root, because $LDAP variable referenciates to root. 
+$direntry = New-Object System.DirectoryServices.DirectoryEntry($LDAP)
+
+#from that directory entry, creates a object DirectorySearcher, to find objects from this point.
+# $direntry is the root, then, the next list will print all objects from this AD.
+
+$dirsearcher = New-Object System.DirectoryServices.DirectorySearcher($direntry)
+$dirsearcher.FindAll()
