8aa643460289fd77a39adcdc2d88b391cecfff67
carlosuy <carlosarmstrong@gmail.com>
2025-01-18 14:42:39 -0300

    se agrega una funcion para mostrar el arbol de directorios

--- function.ps1
+++ function.ps1
@@ -13,3 +13,51 @@ function LDAPSearch {
     return $DirectorySearcher.FindAll()
 
 }
+
+function Get-GroupTree {
+    param (
+        [Parameter(Mandatory)]
+        [object]$Groups,
+        [int]$IndentLevel = 0
+    )
+
+    foreach ($group in $Groups) {
+        # Mostrar el grupo principal
+        $groupName = $group.Properties.cn
+        Write-Output (" " * $IndentLevel + "GRUPO: " + $groupName)
+
+        # Obtener los miembros del grupo
+        $members = $group.Properties.member
+
+        if ($members) {
+            foreach ($member in $members) {
+                # Buscar el objeto del miembro
+                try {
+                    $memberObject = [ADSI]"LDAP://$member"
+                } catch {
+                    Write-Output (" " * ($IndentLevel + 2) + "(Error resolving member: $member)")
+                    continue
+                }
+
+                # Determinar el tipo del miembro
+                $objectClass = $memberObject.Properties["objectClass"]
+
+                if ($objectClass -contains "group") {
+                    # Mostrar el grupo anidado
+                    Write-Output (" " * ($IndentLevel + 2) + ".+" + $($memberObject.Properties["cn"][0]))
+                    # Procesar recursivamente el grupo anidado
+                    Get-GroupTree -Groups @($memberObject) -IndentLevel ($IndentLevel + 4)
+                } elseif ($objectClass -contains "user") {
+                    # Mostrar el usuario
+                    Write-Output (" " * ($IndentLevel + 2) + ".-" + $($memberObject.Properties["cn"][0]))
+                } else {
+                    # Miembro no identificado
+                    Write-Output (" " * ($IndentLevel + 2) + "(Unknown member type: $member)")
+                }
+            }
+        } else {
+            Write-Output (" " * ($IndentLevel + 4) + "(No members found)")
+        }
+    }
+}
+
